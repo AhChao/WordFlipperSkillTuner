84d9bea0c23e371c92dc952e337ddb52931641f2
AhChao <gash612@gmail.com>
2021-06-22 09:51:40 +0800

    [Implement] Add draw svg functions and modify color

---
+++ src/drawSVG.js
@@ -0,0 +1,58 @@
+let drawTarget = "skillPainter";
+let drawWitdh = document.getElementById("skillPainter").clientWidth;
+let lineMargin = drawWitdh/10;
+let lineWidth = drawWitdh/3.5;
+let lineHeight = 100;
+let primaryColor = "#0d6efd";
+let secondaryColor = "#6c757d";
+let warningColor = "#ffc107";
+let textColor = "#000000";
+let textXPadding = drawWitdh/12;
+let textYPadding = drawWitdh/10;
+
+function svgInit()
+{
+    d3.select("#"+drawTarget).append("rect")
+    .attr("x",0)
+    .attr("y",0)
+    .attr("width",drawWitdh)
+    .attr("height",2)
+    .attr("fill",textColor);
+}
+
+function drawUnion(unionNo,text,yPos)
+{
+    let x;
+    let y = yPos;
+    let fillColor;
+    if(unionNo == 1)
+    {
+        x = lineMargin;
+        fillColor = primaryColor;
+    }
+    if(unionNo == 2)
+    {
+        x = lineMargin;
+        fillColor = secondaryColor;
+    }
+    if(unionNo == 3)
+    {
+        x = lineMargin;
+        fillColor = warningColor;
+    }
+
+    let textX = x + textXPadding;
+    let textY = yPos+textYPadding;
+
+    d3.select("#"+drawTarget).append("rect")
+    .attr("x",x)
+    .attr("y",y)
+    .attr("width",lineWidth)
+    .attr("height",lineHeight)
+    .attr("fill",fillColor);
+    d3.select("#"+drawTarget).append("text")
+    .attr("x",textX)
+    .attr("y",textY)
+    .attr("fill",textColor)
+    .text(text);
+}
--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ const ids = ['mainSkill1', 'mainSkill2', 'mainSkill3',
 'unionInitCharge1','unionInitCharge2','unionInitCharge3',
 'unionSkillCharge11','unionSkillCharge12','unionSkillCharge13',
 'unionSkillCharge21','unionSkillCharge22','unionSkillCharge23',
-'unionSkillCharge31','unionSkillCharge32','unionSkillCharge33'];
+'unionSkillCharge31','unionSkillCharge32','unionSkillCharge33',
+'unionSkillChargeTime11','unionSkillChargeTime12','unionSkillChargeTime13',
+'unionSkillChargeTime21','unionSkillChargeTime22','unionSkillChargeTime23',
+'unionSkillChargeTime31','unionSkillChargeTime32','unionSkillChargeTime33'];
 let inputDict = {};
 
 const elements = document.querySelectorAll(ids.map(id => `#${id}`).join(', '));
